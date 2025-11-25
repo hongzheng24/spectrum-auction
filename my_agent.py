1a5ac4bde62e780bab1a3c085d21e05fc295eafa
hongzheng24 <hong_zheng@brown.edu>
2025-11-24 22:35:35 -0500

    feat(working)

--- my_agent.py
+++ my_agent.py
@@ -8,45 +8,263 @@ import os
 import random
 import gzip
 import json
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from collections import deque
 from path_utils import path_from_local_root
 
+# Hyperparams
+GAMMA = 0.95
+EPSILON = 1.0
+LEARNING_RATE = 0.001
+BATCH_SIZE = 64
+MEMORY_SIZE = 10000
+TARGET_UPDATE = 10
+HIDDEN_SIZE = 128 
+DROPOUT = 0.2
+NUM_GOODS = 12
 
-NAME = # TODO: Please give your agent a NAME
+NAME = 'keyreg' # TODO: Please give your agent a NAME
 
 class MyAgent(MyLSVMAgent):
+    def __init__(self, name):
+        super().__init__(name)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.state_size = NUM_GOODS * 4 + 2 # state: [vals, min_bids, cur_prices, eligib, is_national, round_num] * num goods
+        self.num_bid_levels = 5 
+        self.action_size = self.num_bid_levels ** 3
+        self.agent = DQNAgent(self.state_size, self.action_size, device=self.device)
+        self.training = True
+        
+        model_path = path_from_local_root("models/dqn_agent.pth")
+        if self.agent.load(model_path):
+            print(f"Loaded pre-trained model from {model_path}")
+
     def setup(self):
-        #TODO: Fill out with anything you want to initialize each auction
-        pass 
+        self.prev_state = None
+        self.prev_action = None
+        self.round_utils = []
+        self.round_num = 0
+        return
+        
+    def get_state(self):
+        valuations = self.get_valuations()
+        min_bids = self.get_min_bids()
+        cur_prices = min_bids.copy() # cur_prices = self.get_current_prices()
+        eligibility = {good: 1 for good in valuations.keys()} # eligibility = self.get_eligibility()
+        sorted_goods = sorted(valuations.keys())
+
+        state = []
+        max_val = max(valuations.values()) if valuations else 1
+        for good in sorted_goods[:NUM_GOODS]:
+            state.append(valuations.get(good, 0) / max_val)
+            state.append(min_bids.get(good, 0) / max_val)
+            state.append(cur_prices.get(good, 0) / max_val) 
+            state.append(eligibility.get(good, 1))
+        
+        while len(state) < NUM_GOODS * 4:
+            state.extend([0, 0, 0, 0])
+
+        state.append(1 if self.is_national_bidder() else 0)  # Bidder type
+        state.append(self.round_num / 100.0)
+
+        return np.array(state, dtype=np.float32)
     
-    def national_bidder_strategy(self): 
-        # TODO: Fill out with your national bidder strategy
+    def action_to_bids(self, action):
+        valuations = self.get_valuations()
         min_bids = self.get_min_bids()
-        valuations = self.get_valuations() 
-        bids = {} 
-        ...
+        bids = {}
+        sorted_goods = sorted(valuations.items(), key=lambda x: x[1], reverse=True)
+        
+        # Action is int in range(num_bid_levels ** 3)
+        # Each action represents a combination of bid multipliers for each of top 3 items
+        # Bid multipliers on min_bid
+        bid_multipliers = [1.0, 1.05, 1.1, 1.2, 1.5]
+        num_bid_goods = 3 if self.is_national_bidder() else 2
+        
+        for i in range(min(num_bid_goods, len(sorted_goods))):
+            good, val = sorted_goods[i]
+            bid_level = (action // (self.num_bid_levels ** i)) % self.num_bid_levels
+            if bid_level > 0:
+                multiplier = bid_multipliers[bid_level]
+                bid_amount = min_bids[good] * multiplier
+                if val > bid_amount:
+                    bids[good] = bid_amount
+        
         return bids
+    
+    def national_bidder_strategy(self):
+        state = self.get_state()
+        action = self.agent.select_action(state, training=self.training)
+        self.prev_state, self.prev_action = state, action
 
-    def regional_bidder_strategy(self): 
-        # TODO: Fill out with your regional bidder strategy
-        min_bids = self.get_min_bids()
-        valuations = self.get_valuations() 
-        bids = {} 
-        ...
+        return self.action_to_bids(action)
+    
+    def regional_bidder_strategy(self):
+        state = self.get_state()
+        action = self.agent.select_action(state, training=self.training)
+        self.prev_state, self.prev_action = state, action
+        
+        # Filter for proximity goods
+        all_bids = self.action_to_bids(action)
+        proximity = self.get_goods_in_proximity()
+        bids = {good: bid for good, bid in all_bids.items() if good in proximity}
+
         return bids
-
+    
     def get_bids(self):
-        if self.is_national_bidder(): 
+        if self.is_national_bidder():
             return self.national_bidder_strategy()
-        else: 
+        else:
             return self.regional_bidder_strategy()
     
+    def calculate_reward(self):
+        cur_util = self.calc_total_utility()
+        if len(self.round_utils) > 0:
+            reward = cur_util - self.round_utils[-1]
+        else:
+            reward = cur_util
+        
+        self.round_utils.append(cur_util)
+        return reward
+    
     def update(self):
-        #TODO: Fill out with anything you want to update each round
-        pass 
+        self.round_num += 1
+        if self.prev_state is not None and self.training:
+            reward = self.calculate_reward()
+            next_state = self.get_state()
+            done = False
+            self.agent.store_transition(
+                self.prev_state,
+                self.prev_action,
+                reward,
+                next_state,
+                done
+            )
+            loss = self.agent.train()
+            
+            # if loss is not None and self.round_number % 10 == 0:
+            #     print(f"Round {self.round_number}, Loss: {loss:.4f}, Epsilon: {self.dqn_agent.epsilon:.3f}")
 
     def teardown(self):
         #TODO: Fill out with anything you want to run at the end of each auction
         pass 
+
+class DQNNetwork(nn.Module):
+    def __init__(self, state_size, action_size, hidden_size=HIDDEN_SIZE, dropout=DROPOUT):
+        super().__init__()
+
+        self.linear1 = nn.Linear(state_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, action_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        return x
+
+
+class Memory:
+    def __init__(self, capacity):
+        self.memory = deque(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def get_len(self):
+        return len(self.memory)
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, epsilon=EPSILON, gamma=GAMMA, device='cpu'):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.device = device
+
+        self.q_net = DQNNetwork(state_size, action_size).to(device)
+        self.target_net = DQNNetwork(state_size, action_size).to(device)
+        self.target_net.load_state_dict(self.q_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = optim.Adam(self.q_net.parameters(), lr=LEARNING_RATE)
+        self.loss_fn = nn.MSELoss()
+        self.memory = Memory(MEMORY_SIZE)
+        self.steps_done = 0
+
+        
+    def select_action(self, state, training=True):
+        if training and random.random() < self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            with torch.no_grad():
+                state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+                q_values = self.q_net(state_tensor)
+                return q_values.argmax().item()
+    
+    def store_transition(self, state, action, reward, next_state, done):
+        self.memory.push(state, action, reward, next_state, done)
+    
+    def train(self):
+        if self.memory.get_len() < BATCH_SIZE:
+            return None
+        
+        transitions = self.memory.sample(BATCH_SIZE)
+        batch = list(zip(*transitions))
+        
+        state_batch = torch.FloatTensor(batch[0]).to(self.device)
+        action_batch = torch.LongTensor(batch[1]).unsqueeze(1).to(self.device)
+        reward_batch = torch.FloatTensor(batch[2]).unsqueeze(1).to(self.device)
+        next_state_batch = torch.FloatTensor(batch[3]).to(self.device)
+        done_batch = torch.FloatTensor(batch[4]).unsqueeze(1).to(self.device)
+        
+        cur_q = self.q_net(state_batch).gather(1, action_batch)
+        with torch.no_grad():
+            next_q = self.target_net(next_state_batch).max(1)[0].unsqueeze(1)
+            target_q = reward_batch + (1 - done_batch) * self.gamma * next_q
+        
+        loss = self.loss_fn(cur_q, target_q)
+        self.optimizer.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm_(self.q_net.parameters(), 1.0)
+        self.optimizer.step()
+        
+        return loss.item()
+    
+    def update_target_network(self):
+        self.target_net.load_state_dict(self.q_net.state_dict())
+    
+    def save(self, filepath):
+        torch.save({
+            'q_net': self.q_net.state_dict(),
+            'target_net': self.target_net.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'epsilon': self.epsilon,
+            'steps_done': self.steps_done
+        }, filepath)
+    
+    def load(self, filepath):
+        if os.path.exists(filepath):
+            checkpoint = torch.load(filepath, map_location=self.device)
+            self.q_net.load_state_dict(checkpoint['q_net'])
+            self.target_net.load_state_dict(checkpoint['target_net'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            self.epsilon = checkpoint['epsilon']
+            self.steps_done = checkpoint['steps_done']
+            return True
+        return False
 
 ################### SUBMISSION #####################
 my_agent_submission = MyAgent(NAME)
@@ -112,6 +330,8 @@ if __name__ == "__main__":
     # process_saved_game(path_from_local_root("saved_games/2024-04-08_17-36-34.json.gz"))
     # or every file in a directory 
     # process_saved_dir(path_from_local_root("saved_games"))
+
+    print('########## NEW RUN ##############')
     
     ### DO NOT TOUCH THIS #####
     agent = MyAgent(NAME)
